13545ae544da74e96f69bf788c6dfe9742732380
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-05-17 12:34:56 +0200

    Added deprecation notice

--- src/BSplines.jl
+++ src/BSplines.jl
@@ -1,5 +1,10 @@
 module BSplines
 
+function __init__()
+    @warn "The BSplines.jl package has been deprecated in favour of JuliaApproximation/CompactBases.jl"
+    nothing
+end
+
 using RecipesBase
 
 include("knot_sets.jl")
